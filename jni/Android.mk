a6d804634719eeb240473c63a2d50d21cb0fb00f
codetronik <codetronik@gmail.com>
2022-05-14 22:02:52 +0900

    add adb push

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,3 +5,9 @@ LOCAL_MODULE := dump
 LOCAL_SRC_FILES := main.cpp
 
 include $(BUILD_EXECUTABLE)
+
+all: post-build
+
+post-build:
+	adb push ../libs/$(TARGET_ARCH_ABI)/$(LOCAL_MODULE) /data/local/tmp
+	adb shell "chmod 777 /data/local/tmp/$(LOCAL_MODULE)"
